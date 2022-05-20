81a524802b3d529a0222ae42b9038dfeb86b4c0e
vadim <dvv1980@yahoo.com>
2022-05-19 20:23:08 -0400

    Sign_in_page-fixture in test class

--- conftest.py
+++ conftest.py
@@ -7,31 +7,36 @@ from selenium import webdriver
 from constants import login_page_constants as login_const
 from constants import header_constants as header_const
 from constants import global_constants as global_const
+from constants.global_constants import PATH_TO_CHROME_WEBDRIVER, PATH_TO_FIREFOX_WEBDRIVER
 
 from functions.common_functions import CommonFunctions
+from functions.helpers import CHROME, FIREFOX
 from functions.log_in_functions import LogInFunctions
 from functions.register_functions import RegisterFunctions
 from functions.category_page_functions import CategoryPageFunctions
 from functions.shopping_cart_functions import ShoppingCartFunctions
 
 
-@pytest.fixture()
-def start_page():
-    options = Options()
 
-    options.headless = True
-    driver = webdriver.Chrome(options=options, executable_path=global_const.PATH_TO_CHROME_WEBDRIVER)
-    driver.get(global_const.START_PAGE_url)
-    driver.implicitly_wait(time_to_wait=10)
-    yield CommonFunctions(driver)
-    driver.close()
+# @pytest.fixture()
+# def start_page():
+
+#     options = Options()
+#
+#     # options.headless = True
+#     driver = webdriver.Chrome(options=options, executable_path=global_const.PATH_TO_CHROME_WEBDRIVER)
+#     driver.get(global_const.START_PAGE_url)
+#     driver.implicitly_wait(time_to_wait=10)
+#     yield CommonFunctions(driver)
+#     driver.close()
+#
 
 
-@pytest.fixture()
-def login_page(start_page):
-    start_page.click_button_and_verify_new_url(button=header_const.LOGIN_BUTTON_IN_HEADER_xpath,
-                                               url=global_const.LOGIN_PAGE_url)
-    return LogInFunctions(start_page.driver)
+# @pytest.fixture()
+# def login_page(start_page):
+#     start_page.click_button_and_verify_new_url(button=header_const.LOGIN_BUTTON_IN_HEADER_xpath,
+#                                                url=global_const.LOGIN_PAGE_url)
+#     return LogInFunctions(start_page.driver)
 
 
 @pytest.fixture()
--- tests/Sign_in_page/test_log_in.py
+++ tests/Sign_in_page/test_log_in.py
@@ -1,6 +1,13 @@
+import pytest
+
 from constants import login_page_constants as log_const
+from functions.helpers import FIREFOX, CHROME, create_driver
+from constants import global_constants as global_const
+from constants import header_constants as header_const
+from functions.log_in_functions import LogInFunctions
 
 
+@pytest.mark.parametrize("browser_name", [FIREFOX, CHROME])
 class TestLogIn:
     """ Test cases for 'Log In' page.
     Log in data:
@@ -17,6 +24,14 @@ class TestLogIn:
     9. email - valid,  password - valid
     """
 
+    @pytest.fixture(scope="function")
+    def login_page(self, browser_name):
+        driver = create_driver(browser_name)
+        driver.implicitly_wait(time_to_wait=10)
+        driver.get(global_const.START_PAGE_url)
+        yield LogInFunctions(driver)
+        driver.close()
+
     def test1_empty_email_and_empty_password(self, login_page):
         """ Summary: login with empty 'email' field
             Preconditions: get in "Log in" page
@@ -26,6 +41,9 @@ class TestLogIn:
                    3. click 'log in' button in 'log in' page
                    4. verify log in error message 'Login was unsuccessful. Please...' appears
         """
+        login_page.click_button_and_verify_new_url(button=header_const.LOGIN_BUTTON_IN_HEADER_xpath,
+                                                   url=global_const.LOGIN_PAGE_url)
+
         login_page.fill_login_page_fields_and_click(email_data="", password_data="")
 
         login_page.verify_text_partly_present_in_locator(message_locator=log_const.LOGIN_ERROR_MESSAGE_xpath,
@@ -33,6 +51,9 @@ class TestLogIn:
 
     def test2_empty_email_and_valid_password(self, login_page):
         # 2. email - empty,  password - valid
+        login_page.click_button_and_verify_new_url(button=header_const.LOGIN_BUTTON_IN_HEADER_xpath,
+                                                   url=global_const.LOGIN_PAGE_url)
+
         login_page.fill_login_page_fields_and_click(email_data="", password_data=log_const.VALID_PASSWORD)
 
         login_page.verify_text_partly_present_in_locator(message_locator=log_const.LOGIN_ERROR_MESSAGE_xpath,
@@ -40,6 +61,8 @@ class TestLogIn:
 
     def test4_invalid_email_and_empty_password(self, login_page):
         # 4. email - invalid,  password - empty
+        login_page.click_button_and_verify_new_url(button=header_const.LOGIN_BUTTON_IN_HEADER_xpath,
+                                                   url=global_const.LOGIN_PAGE_url)
         login_page.fill_login_page_fields_and_click(email_data=log_const.INVALID_EMAIL, password_data="")
 
         login_page.verify_message(locator=log_const.EMAIL_VALIDATION_ERROR_xpath,
@@ -47,6 +70,9 @@ class TestLogIn:
 
     def test9_valid_email_and_valid_password(self, login_page):
         # 9. email - valid,  password - valid
+        login_page.click_button_and_verify_new_url(button=header_const.LOGIN_BUTTON_IN_HEADER_xpath,
+                                                   url=global_const.LOGIN_PAGE_url)
+
         login_page.fill_login_page_fields_and_click(email_data=log_const.VALID_EMAIL,
                                                     password_data=log_const.VALID_PASSWORD)
         login_page.verify_message(locator=log_const.USER_NAME_IN_HEADER_xpath,
